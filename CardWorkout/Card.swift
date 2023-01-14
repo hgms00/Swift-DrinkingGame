e6fcb0a25a5aaadf6c58483859ad5fa86d39b0c4
Hugo Gabriel <48159371+hgms00@users.noreply.github.com>
2023-01-14 10:50:23 -0300

    Add files via upload

---
+++ CardWorkout/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  CardWorkout
+//
+//  Created by user231920 on 1/8/23.
+//
+
+import UIKit
+
+struct Deck {
+    static var allValues = [
+        UIImage(named: "AD")!,
+        UIImage(named: "AC")!,
+        UIImage(named: "AS")!,
+        UIImage(named: "AH")!,
+    ]
+}
+
